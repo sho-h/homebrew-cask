890b08661e8f8915cc29179b98bf631293aafc3c
Sho Hashimoto <sho.hsmt@gmail.com>
2014-10-09 14:36:44 +0900

    Add libreoffice ja langpack.

---
+++ Casks/libreoffice-langpack-ja.rb
@@ -0,0 +1,9 @@
+class LibreofficeLangpackJa < Cask
+  version '4.3.2'
+
+  sha256 '3d2b6e56ec44177b68143447d2f0156a014da7388e16e8d1c590512028944c6d'
+  url "http://download.documentfoundation.org/libreoffice/stable/#{version}/mac/x86/LibreOffice_#{version}_MacOS_x86_langpack_ja.dmg"
+
+  homepage 'https://www.libreoffice.org/'
+  license :unknown
+end
